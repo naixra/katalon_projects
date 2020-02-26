30128cfbacc52c81811e2b78d7209cf5e04b9c75
naixra <stuff.misc@laposte.net>
2020-02-26 17:04:44 +0100

    Add files via upload

---
+++ Test Suites/Login.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c37253f7-311f-4efc-bb07-c2ca73fafe62</testSuiteGuid>
+   <testCaseLink>
+      <guid>6b174f1a-2428-48cf-92dc-31a9952db7f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Compte_désactivé</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5e1862ce-de44-47bf-bd38-2635b00967fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Compte_bloqué</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b498c800-15c1-400f-bc48-3c78236c523c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Accès_traducteur</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
