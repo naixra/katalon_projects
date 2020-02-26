c79afc495891ef6526973817d0185316f7973dc8
naixra <stuff.misc@laposte.net>
2020-02-26 18:10:36 +0100

    Add files via upload

---
+++ Test Suites/Test.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>906bd4bc-eece-4352-841c-18209a350932</testSuiteGuid>
+   <testCaseLink>
+      <guid>1dc25751-71de-4f29-bf85-08b355ce676f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Concepteur_de_documents</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
